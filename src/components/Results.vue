44b62b39e7c588dce63802b68fbfacbb4d578654
clovissocial <sociale965@gmail.com>
2021-07-22 15:49:26 +0100

    initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="result-block">
+        <p>Reaction Time - {{ score }} ms</p>
+        <h1 class="rank">{{ rank }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 350) {
+            this.rank = 'That was fast!'
+        } else if (this.score < 600) {
+            this.rank = 'Good Reflexes'
+        } else {
+            this.rank = 'Very Slow pace...'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+    
+.result-block {
+    color: #0192a5;
+}
+
+.rank {
+    font-size: 1.4rem;
+    color: #0192a5;
+    font-weight: bold;
+}
+
+</style>
